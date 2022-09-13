a29ac878abf6b620e516a08d1c0199e5ea8bff57
Ray Le <keri.my.le@gmail.com>
2022-09-13 17:41:15 +0000

    Listen for 'submit' events on the journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 var $imagePreview = document.querySelector('#image-preview');
 var $photoURLInput = document.querySelector('#photo-url');
+var $form = document.querySelector('form');
 
 $photoURLInput.addEventListener('input', function (event) {
   $imagePreview.setAttribute('src', $photoURLInput.value);
   if ($photoURLInput.value === '') {
     $imagePreview.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = {
+    title: $form.elements.title.value,
+    photoURL: $form.elements['photo-url'].value,
+    notes: $form.elements.notes.value,
+    entryID: data.nextEntryId
+  };
+  ++data.nextEntryId;
+  data.entries.unshift(formData);
+  $form.reset();
+  $imagePreview.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
