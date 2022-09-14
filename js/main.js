fc037b9eb9bd60bae343f766145263c21d4ac23d
Ray Le <keri.my.le@gmail.com>
2022-09-14 18:12:28 +0000

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -22,3 +22,42 @@ $form.addEventListener('submit', function (event) {
   $form.reset();
   $imagePreview.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
+
+function displayEntry(entry) {
+  var newLi = document.createElement('li');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+  newLi.appendChild(divRow);
+
+  var halfCol1 = document.createElement('div');
+  halfCol1.setAttribute('class', 'column-half');
+  divRow.appendChild(halfCol1);
+
+  var imgDisplay = document.createElement('img');
+  imgDisplay.setAttribute('class', 'entry-img');
+  imgDisplay.setAttribute('src', entry.photoURL);
+  halfCol1.appendChild(imgDisplay);
+
+  var halfCol2 = document.createElement('div');
+  halfCol2.setAttribute('class', 'column-half');
+  divRow.appendChild(halfCol2);
+
+  var h3 = document.createElement('h3');
+  var h3Title = document.createTextNode(entry.title);
+  h3.appendChild(h3Title);
+  halfCol2.appendChild(h3);
+
+  var p = document.createElement('p');
+  var pNotes = document.createTextNode(entry.notes);
+  p.appendChild(pNotes);
+  halfCol2.appendChild(p);
+
+  return newLi;
+}
+
+var entryList = document.querySelector('.entry-list');
+
+for (let i = 0; i < data.entries.length; ++i) {
+  entryList.appendChild(displayEntry(data.entries[i]));
+}
